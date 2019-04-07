f5ffa8e04cbc333f7e0a92d2d654dcfb1154ff10
Jan Dennis Schmutz <janschmutz@hotmail.de>
2019-04-07 02:37:17 +0200

    functioning baseline before blockchein

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,54 @@
 import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { NavController } from '@ionic/angular';
+import * as Web3 from 'web3';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  defBtn: any;
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+  maxLevel: any;
+  minLevel: any;
+  targetLevel: any;
+  datePick: any;
+  pickedDate: any;
+
+  date = new Date();
+  dateSeconds=this.date.getTime();
+  readableDate= formatDate(this.date,"short","en","UTC+1");
+  
+
+
+  onChange(){
+    this.minLevel;
+    if (this.minLevel>=this.maxLevel){
+      this.maxLevel=this.minLevel;
+    }
+    if (this.minLevel>=this.targetLevel){
+      this.targetLevel=this.minLevel;
+    }
+  }
+  onFocus(){
+    this.pickedDate = Date.parse(this.datePick);
+    
+  }
+  onBlur(){
+    if(this.maxLevel<this.minLevel){
+      this.maxLevel=this.minLevel
+    }
+    if (this.minLevel>=this.targetLevel){
+      this.targetLevel=this.minLevel;
+    }
+  }
+
+  
+}
